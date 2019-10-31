e6142a736c52a960fb24288bf295e2673ad64605
Carter Bouley <carterbouley@gmail.com>
2019-10-31 18:33:56 +0000

    thursday evening commit

--- lib.py
+++ lib.py
@@ -15,6 +15,6 @@ class WeatherGetter():
         self.BASE_URL = 'https://api.darksky.net/'
         self.SECRET_KEY = os.getenv("DARKSKY_KEY")
         
-    def weather_getter(self, long, lat):
-        forcast = requests.get(f"{self.BASE_URL}forecast/{self.SECRET_KEY}/{long},{lat}")
+    def weather_getter(self, time):
+        forcast = requests.get(f"{self.BASE_URL}forecast/{self.SECRET_KEY}/52.5200,13.4050,{time}")
         return forcast
